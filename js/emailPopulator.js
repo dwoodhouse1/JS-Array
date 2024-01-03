a9de7e3ab255ae29b96433dd7a83450ab3082f92
dwoodhouse1 <148891593+dwoodhouse1@users.noreply.github.com>
2024-01-03 17:05:16 +0000

    Toast + Email Validation
    Added toasts and email regex

---
+++ js/emailPopulator.js
@@ -0,0 +1,144 @@
+const btnAddEmail = document.getElementById("btn_add-email");
+const emailTextField = document.getElementById("textfield_add-email");
+const emailDropdown = document.getElementById("email-address-dropdown")
+const currentEmailText = document.getElementById("current-email-text");
+
+function populateDropdown() {
+    //alert("test");
+    if (checkEmail()) {
+        let text = emailTextField.value;
+        let newOption = document.createElement("OPTION");
+        let newOptionVal = document.createTextNode(text);
+        
+    
+        newOption.appendChild(newOptionVal);
+        emailDropdown.insertBefore(newOption, emailDropdown.lastChild);
+        emailTextField.value = null;
+    }
+}
+
+function populateEmailText() {
+    let dropDownValue = emailDropdown.options[emailDropdown.selectedIndex].value;
+    currentEmailText.textContent = dropDownValue;
+}
+
+const showError = (input, message) => {
+    
+
+    //adding error class
+    emailTextField.classList.remove("success");
+    emailTextField.classList.add("error");
+
+    //displays the error message in the small tag
+    const error = document.querySelector("small");
+    error.textContent = message;
+}
+
+const showSuccess = (input) => {
+    //get the form-field element
+    
+
+    //remove the success class
+    emailTextField.classList.remove("error");
+    emailTextField.classList.add("success");
+
+    //hide the error message
+    const error = document.querySelector("small");
+    error.textContent = "";
+}
+
+const isEmailValid = (email) => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+}
+
+const checkEmail = () => {
+    let valid = false;
+
+    const email = emailTextField.value.trim();
+
+    
+    if (!isEmailValid(email))
+    {
+        showError(emailTextField, "Email format invalid");
+    }
+    else
+    {
+        showSuccess(emailTextField);
+        valid = true;
+    }
+    return valid;
+}
+
+emailTextField.addEventListener("input", function (e){
+    switch (e.target.id)
+    {
+        case "textfield_add-email":
+            checkEmail();
+            break; 
+    }
+})
+
+
+// Toast Notification with Progress Bar provided by https://codepen.io/alvarotrigo/pen/YzvKNvo
+const button = document.querySelector("#btn_add-email"),
+  toast = document.querySelector(".toast");
+  toastContent = document.querySelector("#toast-circle");
+(closeIcon = document.querySelector(".close")),
+  (progress = document.querySelector(".progress"));
+
+let timer1, timer2;
+
+    console.log("toast");
+    button.addEventListener("click", () => {
+        if (checkEmail()) 
+        {
+            toastContent.classList.add("icon-check");
+            toastContent.classList.remove("icon-x");
+            $(".fas").css("backgroundColor", "blue");
+            generateToast("Success!", "Email Address has been Added!");
+        }
+        else
+        {
+            toastContent.classList.add("icon-x");
+            toastContent.classList.remove("icon-check");
+            $(".fas").css("backgroundColor", "red");   
+            generateToast("Error!", "Email Address is not valid!");   
+        }
+        
+      });
+      
+      closeIcon.addEventListener("click", () => {
+        toast.classList.remove("active");
+      
+        setTimeout(() => {
+          progress.classList.remove("active");
+        }, 300);
+      
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+      });
+
+function generateToast(title, desc)
+{
+    console.log("toast email validated");
+    toast.classList.add("active");
+    progress.classList.add("active");
+        
+    timer1 = setTimeout(() => {
+    toast.classList.remove("active");
+    }, 5000); //1s = 1000 milliseconds
+        
+    timer2 = setTimeout(() => {
+    progress.classList.remove("active");
+    }, 5300);
+
+    messageOne = document.querySelector(".text-1");
+    messageTwo = document.querySelector(".text-2");
+
+    messageOne.textContent = title;
+    messageTwo.textContent = desc;
+}      
+
+btnAddEmail.addEventListener('click', populateDropdown);
+emailDropdown.addEventListener('change', populateEmailText);
