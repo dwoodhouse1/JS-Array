0f38fbecf104773cab1b73983edd2bd197957eff
dwoodhouse1 <148891593+dwoodhouse1@users.noreply.github.com>
2024-01-08 11:06:11 +0000

    All JS Added
    All required JS added for the assignment

--- js/emailPopulator.js
+++ js/emailPopulator.js
@@ -1,14 +1,24 @@
 const btnAddEmail = document.getElementById("btn_add-email");
-
 const emailDropdown = document.getElementById("email-address-dropdown")
 const currentEmailText = document.getElementById("current-email-text");
 const btnAddImage = document.getElementById("btn_add-image");
+const imageDisplayer = document.querySelector("#image-displayer");
 
 let userArray = [];
 
+btnAddImage.disabled = true;
+
+if (currentEmailText.textContent === "Enter an email address above")
+    {
+        imageDisplayer.innerHTML = `
+        <ul>
+            <li>Select an email first to start seeing images here</li>
+        </ul>
+        `;
+    }
+
 function populateDropdown() {
-    //alert("test");
-    if (checkEmail()) {
+    if (checkEmail()) { // if the entered email address has passed validation, populate the dropdown with the email address.
         let text = emailTextField.value;
         let newOption = document.createElement("OPTION");
         let newOptionVal = document.createTextNode(text);
@@ -17,66 +27,102 @@ function populateDropdown() {
         emailDropdown.insertBefore(newOption, emailDropdown.lastChild);
         emailTextField.value = null; // resets the textfield to blank
 
-        createUser(text, currentImgURL);
+        createUser(text);
     }
 }
 
 function populateEmailText() {
     let dropDownValue = emailDropdown.options[emailDropdown.selectedIndex].value;
     currentEmailText.textContent = dropDownValue;
-}
-
-btnAddEmail.addEventListener('click', populateDropdown);
-btnAddImage.addEventListener('click', function () {
-    console.log(userArray.images);
-    let img = addImage(userArray);
-    if (img === undefined)
+    
+    if (dropDownValue === "Please select an email address.")
     {
-        alert("Error, no image available");
+        btnAddImage.disabled = true;
     }
     else
     {
-        document.querySelector("#image-displayer").innerHTML = `
+        btnAddImage.disabled = false;
+    }
+}
+
+btnAddEmail.addEventListener('click', populateDropdown); // click 'Add Email'
+
+btnAddImage.addEventListener('click', function () { // click 'Add Another Image'
+    addImage(currentEmailText.textContent);
+    regenerateImages();
+});
+
+emailDropdown.addEventListener('change', populateEmailText); // value change in the dropdown
+emailDropdown.addEventListener('change', regenerateImages); // value change in the dropdown
+
+// Creates an object literal containing an email and images property. This is then pushed into an array.
+function createUser(userEmail) {
+    let user = {
+        email: `${userEmail}`,
+        images: [],
+        
+    };
+    
+    userArray.push(user);
+}
+
+function addImage(emailText) {
+   
+    if (emailText.length === 0) // Double security measure just in case the user finds a way to access the 'Add Current Images' button without any email values in the array (should never be called)
+    {
+        alert("Error: No emails can be found"); 
+    }
+    else
+    {
+        for (let i = 0; i < userArray.length; i++)
+        {
+            if(userArray[i].email === emailText)
+            {
+                userArray[i].images.push(`<li><img src=${currentImgURL} alt='${currentImgDesc}' width=200 height=200></li>`) // Adds the HTML required to add the image to a selected email address within the userArray
+                
+                fetchImage(); // Refreshes the Image Fetcher with a new image once the current image is stored.
+            }
+            
+        }
+    }
+    
+}
+
+function generateImagesHTML(emailText) { // Returns the HTML of all image values contained within the selected email property
+    let html = "";
+    for (let i = 0; i < userArray.length; i++)
+    {
+        if(userArray[i].email === emailText)
+        {
+            for (const image of userArray[i].images)
+            {
+                html += image;
+            }
+            return html;
+        }
+    }
+}
+
+function regenerateImages() { // Recreates the HTML containing the images for the selected email address in the dropdown.
+    
+
+    imageDisplayer.innerHTML = `
+    <ul>
+        ${generateImagesHTML(currentEmailText.textContent)} 
+    </ul>
+    `;
+
+    if (currentEmailText.textContent === "Please select an email address.")
+    {
+        imageDisplayer.innerHTML = `
         <ul>
-            ${addImage(userArray)}  
+            <li>Select an Email Address to see Images.</li>
         </ul>
         `;
     }
     
-});
-
-emailDropdown.addEventListener('change', populateEmailText);
-
-function createUser(userEmail, userImage) {
-    let user = {
-        email: `${userEmail}`,
-        images: [`${userImage}`],
-         //`${userImage}` stays blank until user selects an image
-    };
-    //user.images.push("hello");
-    userArray.push(user);
-    console.log(user);
-    console.log(userArray);
+   
 }
 
-function addImage(arr) {
-    let html = "";
-    console.log(arr);
-    if (arr.length === 0)
-    {
-        alert("Error, no emails have been entered");
-        html = "";
-    }
-    else
-    {
-        for (let i = 0; i < arr.length; i++)
-        {
-            html += `<li><img src=${arr[i]} width=200 height=200></li>`
-            console.log(html);
-        }
-        return html;
-    }
 
-    
-}
 
--- js/imageFetch.js
+++ js/imageFetch.js
@@ -6,36 +6,27 @@ const imageContainer = document.getElementById("image-fetcher__image-container")
 const btnRandomImage = document.getElementById("random-img");
 const apiKey = 'tJTJmyfzjWKJEFP4tUYl3hQTTCLEPHtvnoQv3yvWp9k';
 const apiURL = 'https://api.unsplash.com/photos/random?client_id=';
-const labelURL = document.getElementById("label-url");
 
-let fetchedImage;
-let imageList = [];
 let userAndImages = [];
 let currentImgURL = "";
 let currentImgDesc = "";
-let originalImgURL = "";
-
-function fetchData(url) { // returns a promise once data is retrieved from the web server and passed as json
-    return fetch(url)
-        .then(checkStatus)
-        .then(response => response.json())
-        .then(json => imageList = json)
-        //.then(log => console.log(imageList))
-        .catch(error => console.log("Error, there was a problem", error))    
-}
-
 
 fetchData(`${apiURL}${apiKey}`) // full URL: https://api.unsplash.com/photos/random?client_id=tJTJmyfzjWKJEFP4tUYl3hQTTCLEPHtvnoQv3yvWp9k
     .then(data => generateImage(data.urls.small, data.alt_description))
-    
 
+
+    function fetchData(url) { // returns a promise once data is retrieved from the web server and passed as json
+    return fetch(url)
+        .then(checkStatus)
+        .then(response => response.json())
+        .catch(error => console.log("Error, there was a problem", error))    
+}
 
 function generateImage(data, desc) { // Original Image on page load
     const html = `<img src='${data}' alt='${desc}' height='200' width='200'>`;
     imageContainer.innerHTML = html;
-    //userArray.push(html);
-    originalImgURL = data;
-    //createEmail(imageList.urls.small, currentImgURL);
+    currentImgURL = data;
+    currentImgDesc = desc;
 }
 
 btnRandomImage.addEventListener('click', fetchImage);
@@ -43,7 +34,6 @@ btnRandomImage.addEventListener('click', fetchImage);
 function fetchImage() { // Grabs another image from picsum and places it in the img tag in the img container
     const img = imageContainer.querySelector("img");
     
-
     fetchData(`${apiURL}${apiKey}`)
         .then(data => {
             console.log(data);
@@ -55,14 +45,12 @@ function fetchImage() { // Grabs another image from picsum and places it in the 
             currentImgDesc = data.alt_description;
             console.log(currentImgURL);
         });
-        console.log(currentImgURL);   
-        // console.log(currentImgURL);
-        // labelURL.textContent = currentImgURL;
-        // imageList.push(currentImgURL);
-        // console.log(imageList);
+        console.log(currentImgURL);
+        console.log(currentImgDesc);   
+        
 }
 
-function checkStatus(response) {
+function checkStatus(response) { // If the fetch response is sucessful, a resolved promise is returned, else its rejected.
     if (response.ok) 
     {
         return Promise.resolve(response);
@@ -73,19 +61,5 @@ function checkStatus(response) {
     }
 }
 
-function createEmail(address, image) {
-    let id = address;
-    let div = document.createElement("div");
-    div.setAttribute("id", id);
-    document.getElementById("image-displayer").appendChild(div);
 
-    document.getElementById(id).innerHTML = `<img src=${id}>`
-}
 
-// labelURL.addEventListener('click', test);
-
-// function test()
-// {
-//     console.log("is this on?");
-//     console.log(currentImgURL);
-// }
